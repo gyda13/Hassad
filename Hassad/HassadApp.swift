6f97c260570bc8451e79fd676e080ac1e5dedf37
gyda13 <gyda2100@gmail.com>
2023-02-17 03:57:41 +0300

    Initial Commit

---
+++ Hassad/HassadApp.swift
@@ -0,0 +1,17 @@
+//
+//  HassadApp.swift
+//  Hassad
+//
+//  Created by gyda almohaimeed on 26/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct HassadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
