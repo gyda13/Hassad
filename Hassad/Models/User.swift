ee6f19acc7af1be6c0c07a353369e3702df30135
gyda13 <gyda2100@gmail.com>
2023-02-18 00:49:51 +0300

    first commit

---
+++ Hassad/Models/User.swift
@@ -0,0 +1,21 @@
+//
+//  User.swift
+//  Hassad
+//
+//  Created by gyda almohaimeed on 26/07/1444 AH.
+//
+
+import Foundation
+
+final class User: Codable, Identifiable {
+  var id: UUID?
+  var businessname: String
+  var email: String
+
+  init(id: UUID? = nil, businessname: String, email: String) {
+    self.id = id
+    self.businessname = businessname
+    self.email = email
+  }
+}
+
